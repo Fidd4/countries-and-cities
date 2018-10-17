909bf44d29d8640d31e656e5501b9caadeba1984
Fidd4 <ideasotoole@gmail.com>
2018-10-17 20:05:37 +0300

    Updates

---
+++ precache-manifest.51eaf750311d4d978e06b8f799418b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b02603a6c2ed273546a",
+    "url": "/countries-and-cities/static/js/runtime~main.7b02603a.js"
+  },
+  {
+    "revision": "8fdbf06e7dce8885b824",
+    "url": "/countries-and-cities/static/js/main.8fdbf06e.chunk.js"
+  },
+  {
+    "revision": "78412eec9668b578f4c5",
+    "url": "/countries-and-cities/static/js/1.78412eec.chunk.js"
+  },
+  {
+    "revision": "8fdbf06e7dce8885b824",
+    "url": "/countries-and-cities/static/css/main.336af5a0.chunk.css"
+  },
+  {
+    "revision": "78412eec9668b578f4c5",
+    "url": "/countries-and-cities/static/css/1.27814fa8.chunk.css"
+  },
+  {
+    "revision": "259265758086f5d8e13a7cdfbea36f12",
+    "url": "/countries-and-cities/index.html"
+  }
+];
